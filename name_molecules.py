6316ab18cac8e42d61fc3406db353ff75da959c9
jacob-milton <jrmilton@lbl.gov>
2024-02-14 15:23:57 -0800

    massive overhaul

--- name_molecules.py
+++ name_molecules.py
@@ -10,14 +10,14 @@ from pymatgen.analysis.graphs import MoleculeGraph
 from pymatgen.analysis.local_env import OpenBabelNN
 # from mc_analysis import ReportGenerator
 import glob
-import pickle
+# import pickle
 from monty.serialization import loadfn, dumpfn
 import os
 import copy
 import networkx as nx
 import time
 import csv
-import sqlite3
+# import sqlite3
 import copy
 import operator
 
@@ -30,93 +30,69 @@ writes an excel file for the desired reactions of the form name1 + name2 => name
 and outputs those concentrations as text files. 
 """
 
-
-# def find_molecules_from_mpculeids(participants, mpculeid_to_mol_entry):
-#     """
-#     Converts a dictionary associating reaction "side" descriptors ("reactants" and/or "products") to
-#     reactant and product mpculeids into a dictionary associating those descriptors with
-#     lists of pymatgen Molecule objects.
-
-#     participants: a dictionary with keys limited to "reactants" and/or "products," 
-#                   where each value is a list of mpculeids representing reactants or products.
-#     mpculeid_to_mol_entry: a dictionary associating each mpculeid with its corresponding pymatgen Molecule object.
-
-#     Returns: new_dict, a new dictionary with the same keys ("reactants" and/or "products") 
-#              but updated values, where each value is a list of pymatgen Molecule objects associated with the mpculeids.
-#              Raises a KeyError if an mpculeid is not found in the dictionary.
-#     """
+def validate_species_to_name(species_to_name):
+    species_graph = species_to_name.get("nx_graph", False)
+    species_mol = species_to_name.get("molecule", False)
     
-#     new_dict = {}
+    if not species_graph or not species_mol:
+        raise ValueError("Missing required keys 'nx_graph' or 'molecule' in species_to_name.")
     
-#     for key, side in participants.items():
-        
-#         molecule_list = [mpculeid_to_mol_entry[mpculeid] for mpculeid in side]
-#         new_dict[key] = molecule_list
-        
-#     return new_dict
+    return species_graph, species_mol
 
-# Function has been tested and passed the provided test cases
-# Test cases covered scenarios such as basic conversion, missing mpculeids, and empty input
-    
-def name_molecule(mol, func_group_dict):
-    """
-    Takes a pymatgen Molecule object and generates a "name" for that object based
-    on the functional groups present. Any atoms that are not associated with a 
-    functional group will be be added to the name; this also means that species
-    with no functional groups present will simply return their composition.
-    
-    Parameters
-    ----------
-    mol : pymatgen Molecule object
-        The species we wish to name
-    func_group_dict : dictionary
-        A dictionary whos keys are the names of functional groups and values are
-        the Molecule objects associated with those groups
-
-    Returns
-    -------
-    name : string
-        The generated "name" of the species
-
-    """
-    mol_graph = nx.Graph(mol.graph)
-    mol_copy = copy.deepcopy(mol_graph)
-    name = ""
-    for n, group in func_group_dict.items(): 
-        group_undirected_graph = nx.Graph(MoleculeGraph.with_local_env_strategy(group, OpenBabelNN(order = False)).graph) #Creates a Networkx (undirected) Graph object from func Molecule object
-        while functional_group_present(mol_copy, group_undirected_graph)[0]: #want to repeat until func group is no longer present
-            if n not in name:
-                name = name + n + '_' #if present, add func group name to name
-            else: #if the functional group is already in the name, we just increase the number present
-                if name[-2].isnumeric():
-                    new_number = int(name[-2]) + 1
-                    name = name[0:len(name)-2]+ str(new_number) + '_'
-                else:
-                    name = name[0:len(name)-1] + '2_'
-            mappings = list(functional_group_present(mol_copy, group_undirected_graph)[1]) #points to location of functional group in molecule
-            for atom_index in mappings[0].keys():
-                mol_copy.remove_node(atom_index)  #and remove the group
-    if name and mol_copy: #i.e. if we have added some functional groups to the name but there remain atoms not in functional group
-        comp = ""
-        for node in mol_copy.nodes(data = True): #each node is a 2-tuple containing the index of that node and a dictionary with data related to that node
-            species = node[1]['specie']
-            if species in comp:
-                species_index = comp.index(species)
-                new_number = int(comp[species_index+1]) + 1
-                comp = comp[0:len(comp)-1]+str(new_number)
-            else:
-                comp = comp + species + "1"
-        name = name + comp + '_'
-    elif name and not mol_copy: #i.e. our functional groups encompass all atoms in the molecule
-        pass
-    else: #i.e. the molecule contains no functional groups
-        name = str(mol.molecule.composition).replace(" ", "")
-        name = name + '_'
-    if mol.molecule.charge == 1: #add charges to the end of the names
-        name = name + "+" + str(mol.molecule.charge)
+def update_species_name(species_name, name, number):
+    if name not in species_name:
+        species_name += name + '_'
     else:
-        name = name + str(mol.molecule.charge)
-    return name
+        if number:
+            current_number_present = int(species_name[-2])
+            new_number = current_number_present + 1
+            species_name = species_name[:-2] + str(new_number) + '_'
+        else:
+            species_name = species_name[:-1] + '2_'
+    
+    return species_name
+
+def update_remaining_species_graph(remaining_species_graph, func_group_locations):
+    for atom_index in func_group_locations[0].keys():
+        remaining_species_graph.remove_node(atom_index)
+    
+    return remaining_species_graph
+
+def update_species_name_with_atom_composition(species_name, name, remaining_species_graph):
+    if species_name and remaining_species_graph:
+        atom_composition = ""
+        for node in remaining_species_graph.nodes(data=True):
+            element = node[1]['specie']
+            if element in atom_composition:
+                element_index = atom_composition.index(element)
+                old_number = int(atom_composition[element_index + 1])
+                new_number = old_number + 1
+                atom_composition = atom_composition[:-1] + str(new_number)
+            else:
+                atom_composition += element + "1"
+        species_name += name + atom_composition + '_'
+    
+    return species_name
+
+def generate_species_name(species_to_name, func_group_dict):
+    species_graph, species_mol = validate_species_to_name(species_to_name)
+    remaining_species_graph = copy.deepcopy(species_graph)
+    species_name = ""
+
+    for name, group_graph in func_group_dict.items():
+        func_group_present = functional_group_present(remaining_species_graph, group_graph)[0]
+        func_group_locations = list(functional_group_present(remaining_species_graph, group_graph)[1])
+        while func_group_present:
+            species_name = update_species_name(species_name, name, species_name[-2].isnumeric())
+            remaining_species_graph = update_remaining_species_graph(remaining_species_graph, func_group_locations)
+    
+    species_name = update_species_name_with_atom_composition(species_name, name, remaining_species_graph) \
+                    if species_name else str(species_mol.molecule.composition).replace(" ", "") + '_'
+
+    charge_suffix = "+" if species_mol.molecule.charge == 1 else str(species_mol.molecule.charge)
+    species_name += charge_suffix
+
+    return species_name
 
 def functional_group_present(mol_graph, func):
     """
@@ -146,7 +122,7 @@ def functional_group_present(mol_graph, func):
     isomorphism = nx.isomorphism.GraphMatcher(mol_graph, func, node_match = nm)
     return isomorphism.subgraph_is_isomorphic(), isomorphism.subgraph_isomorphisms_iter()
 
-def stereoisomer_test(name_dict, mol_entry_id, mol_name):
+def stereoisomer_test(stereoisomer_list, name):
     """
     Structural isomers--species with the same formula but different graphs--will return
     the same name when the name_molecule function is called. This function
@@ -170,73 +146,71 @@ def stereoisomer_test(name_dict, mol_entry_id, mol_name):
         and values are entryids
 
     """
-    stereos = {}
-    if mol_name in name_dict and mol_entry_id != name_dict[mol_name]: #i.e. if name already in dictionary but graphs of molecules are different
-        old_id = name_dict[mol_name]
-        name_1 = mol_name + '_#1'
-        name_2 = mol_name + '_#2'
-        stereos.update({name_1: old_id, name_2: mol_entry_id})
-    elif mol_name + '_#1' in name_dict and mol_entry_id != name_dict[mol_name + '_#1']: #i.e. if multiple stereoisomers are already in the dictionary
-        current_max = 3
-        while mol_name + '_#' + str(current_max) in name_dict and mol_entry_id != name_dict[mol_name + '_#' + str(current_max)]:
-            current_max += 1
-        new_name = mol_name + '_#' + str(current_max)
-        stereos[new_name] = mol_entry_id
-    return stereos
-
-def write_reaction(reaction_dict, mpculid_dict): #convert to strings of the appropriate format for kinetiscope
-    """
-    Kinetiscope can read in reactions of the format name1 + name2 => name3
-    + name4. This function uses the names we've generated to write strings for
-    reactions of this format.
-
-    Parameters
-    ----------
-    reaction_dict : dictionary
-        dictionary whose keys are "reactants" and "products" and whose values
-        are a list of mpculeids associated with a given reaction
-    mol_entry_id : dictionary
-        dictionary associating each mpculeid with its name for fast lookup
-
-    Returns
-    -------
-    rxn_eqn: string
-        the desired reaction written in the form: name1 + name2 => name3
-        + name4
-    """
-    rxn_eqn = ""
-    if len(reaction_dict["reactants"]) == 1:
-        name = mpculid_dict[reaction_dict["reactants"][0]]
-        rxn_eqn = name + " "
+    new_stereos_list = []
+    if len(stereoisomer_list) == 1: #i.e. if name already in dictionary but graphs of molecules are different
+        name_1 = name + '_#1'
+        name_2 = name + '_#2'
+        new_stereos_list.append(name_1)
+        new_stereos_list.append(name_2)
     else:
-        first_reactant = reaction_dict["reactants"][0]
-        first_name = mpculid_dict[first_reactant]
-        rxn_eqn = first_name + " + "
-        second_reactant = reaction_dict["reactants"][1]
-        second_name = mpculid_dict[second_reactant]
-        if first_name == second_name:
-            # print(first_name)
-            rxn_eqn = "2 " + first_name + " " #if reaction is A + A -> B + C we have to write 2A rather than A + A
-            # print(rxn_eqn)
-        else:
-            rxn_eqn = rxn_eqn + second_name + " "
-    rxn_eqn = rxn_eqn + "=> "
-    if len(reaction_dict["products"]) == 1:
-        name = mpculid_dict[reaction_dict["products"][0]]
-        rxn_eqn = rxn_eqn + name
-    else:
-         first_product = reaction_dict["products"][0]
-         first_name = mpculid_dict[first_product]
-         # rxn_eqn = rxn_eqn + first_name + " + "
-         second_product = reaction_dict["products"][1]
-         second_name = mpculid_dict[second_product]
-         if first_name == second_name:
-             # print(first_name)
-             rxn_eqn += "2 " + first_name #if reaction is A + A -> B + C we have to write 2A rather than A + A
-             # print(rxn_eqn)
-         else:
-             rxn_eqn = rxn_eqn + first_name + " + " + second_name
-    return rxn_eqn
+        current_max_num = str(len(stereoisomer_list))
+        new_isomer = name + '#_' + current_max_num
+        new_stereos_list.append(new_isomer)
+    return new_stereos_list
+
+# def write_reaction(reaction_dict, mpculid_dict): #convert to strings of the appropriate format for kinetiscope
+#     """
+#     Kinetiscope can read in reactions of the format name1 + name2 => name3
+#     + name4. This function uses the names we've generated to write strings for
+#     reactions of this format.
+
+#     Parameters
+#     ----------
+#     reaction_dict : dictionary
+#         dictionary whose keys are "reactants" and "products" and whose values
+#         are a list of mpculeids associated with a given reaction
+#     mol_entry_id : dictionary
+#         dictionary associating each mpculeid with its name for fast lookup
+
+#     Returns
+#     -------
+#     rxn_eqn: string
+#         the desired reaction written in the form: name1 + name2 => name3
+#         + name4
+#     """
+#     rxn_eqn = ""
+#     if len(reaction_dict["reactants"]) == 1:
+#         name = mpculid_dict[reaction_dict["reactants"][0]]
+#         rxn_eqn = name + " "
+#     else:
+#         first_reactant = reaction_dict["reactants"][0]
+#         first_name = mpculid_dict[first_reactant]
+#         rxn_eqn = first_name + " + "
+#         second_reactant = reaction_dict["reactants"][1]
+#         second_name = mpculid_dict[second_reactant]
+#         if first_name == second_name:
+#             # print(first_name)
+#             rxn_eqn = "2 " + first_name + " " #if reaction is A + A -> B + C we have to write 2A rather than A + A
+#             # print(rxn_eqn)
+#         else:
+#             rxn_eqn = rxn_eqn + second_name + " "
+#     rxn_eqn = rxn_eqn + "=> "
+#     if len(reaction_dict["products"]) == 1:
+#         name = mpculid_dict[reaction_dict["products"][0]]
+#         rxn_eqn = rxn_eqn + name
+#     else:
+#          first_product = reaction_dict["products"][0]
+#          first_name = mpculid_dict[first_product]
+#          # rxn_eqn = rxn_eqn + first_name + " + "
+#          second_product = reaction_dict["products"][1]
+#          second_name = mpculid_dict[second_product]
+#          if first_name == second_name:
+#              # print(first_name)
+#              rxn_eqn += "2 " + first_name #if reaction is A + A -> B + C we have to write 2A rather than A + A
+#              # print(rxn_eqn)
+#          else:
+#              rxn_eqn = rxn_eqn + first_name + " + " + second_name
+#     return rxn_eqn
     
 # def generate_name_key(network_loader, key_dict, species_report_path):
 #     """
@@ -267,36 +241,45 @@ def write_reaction(reaction_dict, mpculid_dict): #convert to strings of the appr
 
 #     dumpfn(data, path)
 
-# start = time.time()
-# folder = input("Please input the name of the folder where you want to store your files: ")
+start = time.time()
+os.chdir(r"G:\My Drive\Kinetiscope\import_test_021424\func_groups")
+# folder = "to_import"
 # original_directory = os.getcwd()
 # new_dir = os.path.join(original_directory, folder)
 # os.mkdir(new_dir)
+# excel_dir = os.path.join(original_directory, folder)
 
-# print('Associating functional groups with their Molecule objects...')
-# func_group_dict = {} #take a group of xyz files associated with our functional groups and generate a dictionary associating the molecule graph of that functional group with its name
-# for filename in glob.glob('*.xyz'):
-#     mol = Molecule.from_file(filename)
-#     name = filename.replace('.xyz', '')
-#     func_group_dict[name] = mol
-# print('Done!')
+print('Associating functional groups with their Molecule objects...')
 
+func_group_dict = {} #take a group of xyz files associated with our functional groups and generate a dictionary associating the molecule graph of that functional group with its name
 
-mpcule_id_molecule_dict = {}
-
-for entry in mol_entries:
+for filename in glob.glob('*.xyz'):
+  
+    func_group = Molecule.from_file(filename)  # pymatgen Molecule
+    func_group_mol_graph = MoleculeGraph.with_local_env_strategy(func_group, OpenBabelNN(order = False))  # pymatgen mol_graph
+    func_group_multigraph = func_group_mol_graph.graph #networkx multigraph
+    group_undirected_graph = nx.Graph(func_group_multigraph) #networkx undirected graph
+    name = filename.replace('.xyz', '')
+    func_group_dict[name] = group_undirected_graph
     
-    mpcule_id = entry.entry_id
-    
-    if mpcule_id: #the electron species has no mpcule_id so we catch it here--what do we do with it?
-        mpcule_id_molecule_dict[mpcule_id] = entry
-        print(mpcule_id_molecule_dict)
-        break
-    
-# # n = 0
-# name_dict = {}
+print('Done!')
+
+os.chdir(r"G:\My Drive\Kinetiscope\import_test_021424")
+json = "mpcule_id_molecule_association.json"
+mpcule_id_molecule_dict = loadfn(json)
+
+mpcule_name_dict = {}
+stereo_dict = {}
+names = set()
+
+for mpculeid, mol_dict in mpcule_id_molecule_dict:
+    test_name = generate_species_name(mol_dict)
+    if test_name in stereo_dict:
+        current_stereos = stereo_dict.get(test_name)
+    else:
+        mpcule_name_dict[mpculeid] = test_name
+        stereo_dict[test_name] = [test_name]
 # reactions_added = set()
-# entry_ids = set()
 # reactions = []
 
 # print('Naming Molecules...')
