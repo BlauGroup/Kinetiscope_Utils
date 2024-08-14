7ccbc66b83c6e85bbb9b1080170141cd25b724a3
jacob-milton <jrmilton@lbl.gov>
2024-08-13 18:58:37 -0700

    Revert "major refactor to make this mess readable"
    This reverts commit 3725b1f83d0e4df6739cdc5eaad440e1ff41e190.

--- kinetiscope_reaction_writing_utilities.py
+++ kinetiscope_reaction_writing_utilities.py
@@ -7,100 +7,6 @@ Created on Thu Aug  8 14:24:24 2024
 
 from Rxn_classes import Kinetiscope_Reaction
 
-class ReactionDataStorage:
-    def __init__(self, name_mpculeid_dict,marker_species_dict,excitation_set,absorption_dict=None):
-
-        self.mpculeid_dict = self.build_mpculeid_dict(name_mpculeid_dict)
-        self.marker_species_dict = marker_species_dict
-        self.excitation_set = excitation_set
-        self.rate_constant_dict = self.create_rate_constant_dict(absorption_dict)
-    
-    def build_mpculeid_dict(self, name_mpculeid_dict):
-        return {mpculeid: name for name, mpculeid in name_mpculeid_dict.items()}
-    
-    def create_rate_constant_dict(self, absorption_dict=None):
-        """
-        Creates and returns a nested dictionary to hold rate constants for various reaction types.
-
-        The dictionary is organized into two main categories: 'ionization' and 'chemical'.
-        Each category contains specific subcategories for different reaction processes.
-
-        Parameters
-        ----------
-        absorption_dict : dict, optional
-            A dictionary mapping molecular IDs to absorption rate constants. 
-            If provided, these values will be added to the 'absorption' subcategory.
-
-        Returns
-        -------
-        dict
-            A dictionary with the following structure:
-            - 'ionization':
-                - 'absorption': {<molecular_id>: <rate_constant>, ...}
-                - 'electron_ionization': {
-                    'eV_80': 1.4e+15,
-                    'eV_55': 1.2e+15,
-                    'eV_30': 8.5e+14,
-                    'LEE': 1.7e+14
-                }
-                - 'recombination': {'all': 4.1e+13}
-                - 'attachment': {'positive': 4.1e+13, 'neutral': 3.1e+12}
-            - 'chemical':
-                - '1st_order': 6.2e+12
-                - '2nd_order': 1.3e+12
-                
-        all values are the bottom of the dict are floats written in scientific
-        notation. 
-        
-        """
-        def add_absorption_values(rate_constant_dict, absorption_dict):
-            """
-            Adds a dictionary of absorption rate constants to the rate_constant_dict.
-
-            Parameters
-            ----------
-            rate_constant_dict : dict
-                The rate constant dictionary created by create_rate_constant_dict.
-            absorption_dict : dict
-                A dictionary mapping molecular IDs to absorption rate constants.
-                Example: {"4864aee73a83d357c31fadd50b81e3cd-C10H20O2-0-1": 1.4e-01}
-
-            """
-            if absorption_dict:
-                rate_constant_dict["ionization"]["absorption"].update(absorption_dict)
-            
-        rate_constant_dict = {
-            "ionization": {
-                "absorption": {},
-                "electron_ionization": {
-                    "eV_80": 1.4e+15,
-                    "eV_55": 1.2e+15,
-                    "eV_30": 8.5e+14,
-                    "LEE": 1.7e+14,
-                },
-                "recombination": {"all": 4.1e+13},
-                "attachment": {"positive": 4.1e+13, "neutral": 3.1e+12},
-            },
-            "chemical": {
-                "1st_order": 6.2e+12,
-                "2nd_order": 1.3e+12,
-            }
-        }
-
-        if absorption_dict:
-            add_absorption_values(rate_constant_dict, absorption_dict)
-
-        return rate_constant_dict
-
-def build_reaction_parameter_dict(rate_constant_key, superclass, reaction_order, reactants_to_add=None, products_to_add=None):
-    reaction_parameter_dict = {
-        "rate_constant_key":rate_constant_key,
-        "superclass":superclass,
-        "reaction_order":reaction_order,
-        "reactants_to_add":reactants_to_add,
-        "products_to_add":products_to_add}
-    return reaction_parameter_dict
-        
 def add_reactants(base_name, added_reactants):
     return " + ".join(added_reactants + [base_name])
 
@@ -142,4 +48,3 @@ def replace_tag_with_shorthand(marker_species, shorthand_dict):
             tag = tag.replace(longhand, shorthand)
     marker_species[-1] = tag
     return marker_species
-
