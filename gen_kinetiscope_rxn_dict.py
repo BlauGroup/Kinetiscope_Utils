c7c9b21bc20c204e864fe145728befaa1b953957
jacob-milton <jrmilton@lbl.gov>
2024-08-13 20:30:42 -0700

    Revert "removing duplicate reactions"
    This reverts commit 5c79b9429297f692cf5db0be5b12e749c1801097.

--- gen_kinetiscope_rxn_dict.py
+++ gen_kinetiscope_rxn_dict.py
@@ -9,14 +9,14 @@ __version__ = '1.1.0'
 import os
 import sys
 from monty.serialization import loadfn, dumpfn
-from kinetiscope_reaction_writing_utilities import ReactionDataStorage
+# from Rxn_classes import HiPRGen_Reaction, Kinetiscope_Reaction
+# import sys
 from write_kinetiscope_ionization_reactions import handle_ionization_reactions
 from write_kinetiscope_chemical_reactions import (
 handle_phase1_chemical_reactions,
 handle_phase2_chemical_reactions
 )
 import csv
-import re
 
 def collect_lists_from_nested_dict(d):
     collected_items = []
@@ -54,14 +54,11 @@ def reaction_has_duplicate_species(k_rxn):
     return reactants_are_duplicates or products_are_duplicates
 
 def modify_kinetiscope_name(k_rxn, species_list, side_identifier):
-    # Normalize whitespace around "=>"
-    sides = re.split(r"\s*=>\s*", k_rxn.kinetiscope_name)
-    
+    sides = k_rxn.kinetiscope_name.split("=>")
     if side_identifier == "reactants":
         sides[0] = " + ".join(species_list)
     else:
         sides[1] = " + ".join(species_list)
-    
     return " => ".join(sides)
 
 # def star_test(species_list):
@@ -176,102 +173,80 @@ HiPRGen_reaction_list = loadfn(full_rxns)
 # name_mpculeid_file = "name_test_mpculeid_080624.json"
 name_mpculeid_file = "name_full_mpculeid_080624.json"
 name_mpculeid_dict = loadfn(name_mpculeid_file)
-# mpculeid_name_dict = {mpculeid: name for name, mpculeid in name_mpculeid_dict.items()}
+mpculeid_name_dict = {mpculeid: name for name, mpculeid in name_mpculeid_dict.items()}
 
-absorption_rate_constants = {
-    "4864aee73a83d357c31fadd50b81e3cd-C10H20O2-0-1":1.4e-01,
-    "00a7dcc352b0d613f58e850935bf5609-C10H14O1-0-1":1.0e-01,
-    "bfed458e642b8daa1eab6bc02d5e5682-C18H15S1-1-1":1.8e-01,
-    "9a8a88b8b92c714d7f65b8526ffabc7a-C4F9O3S1-m1-1":5.8e-01,
-    "17f31f89123edbaa0e3b9c7eb49d26f3-C8H4N1O2-m1-1":1.5E-01
-}
-
-# rate_constant_dict = create_rate_constant_dict(absorption_rate_constants)
+rate_constant_dict = { #values calculated in excel and taken as scientific notation values
+    "ionization":
+        {"absorption":{
+            "4864aee73a83d357c31fadd50b81e3cd-C10H20O2-0-1":1.4e-01,
+            "00a7dcc352b0d613f58e850935bf5609-C10H14O1-0-1":1.0e-01,
+            "bfed458e642b8daa1eab6bc02d5e5682-C18H15S1-1-1":1.8e-01,
+            "9a8a88b8b92c714d7f65b8526ffabc7a-C4F9O3S1-m1-1":5.8e-01,
+            "17f31f89123edbaa0e3b9c7eb49d26f3-C8H4N1O2-m1-1":1.5E-01},
+         "electron_ionization":{
+             "eV_80":1.4e+15,
+             "eV_55":1.2e+15,
+             "eV_30":8.5e+14,
+             "LEE":1.1E+14},
+         "recombination":{
+             "all": 4.1e+13},
+         "attachment":{
+             "positive":4.1e+13,
+             "neutral":3.1e+12}
+         },
+    "chemical":
+        {"1st_order":6.2e+12,
+         "2nd_order":1.3e+12}
+        }
     
-marker_species_dict = {
+marker_species_shorthand = {
     "radical_cation":"rc",
     "radical_anion":"ra",
     "neutral_radical":"nr",
     "cation":"c",
     "anion":"a",
+    # "radical":"r",
     "neutral":"n",
-    "proton_coupled_electron_transfer":"PCET"
-}
+    "proton_coupled_electron_transfer":"PCET"}
+
+
 
 excitation_set = set()
+# LEE_H_rxn = None
+# LEE_collision_name = "LEE => TE"
+# LEE_rate_constant = 5.1E+14
+# LEE_order = 1
+# LEE_marker_species = None
 
-reaction_writing_data = ReactionDataStorage(name_mpculeid_dict, marker_species_dict, excitation_set, absorption_rate_constants)
+# LEE_rxn = \
+#     build_rxn_object(LEE_H_rxn, LEE_collision_name, LEE_rate_constant, LEE_order, LEE_marker_species)
 
 for rxn_list in HiPRGen_reaction_list["ionization"].values():
     for HiPRGen_rxn in rxn_list:
         ionization_reaction_list = \
-            handle_ionization_reactions(HiPRGen_rxn, reaction_writing_data)
+            handle_ionization_reactions(HiPRGen_rxn, mpculeid_name_dict, rate_constant_dict)
         kinetiscope_reaction_list.extend(ionization_reaction_list)
 
-# chemical_reaction_list = collect_lists_from_nested_dict(HiPRGen_reaction_list["chemical"])
+chemical_reaction_list = collect_lists_from_nested_dict(HiPRGen_reaction_list["chemical"])
 
-# for H_rxn in chemical_reaction_list:
-#     if H_rxn.phase == 1:
-#         chemical_reaction_list, excitation_set = \
-#             handle_phase1_chemical_reactions(H_rxn, mpculeid_name_dict, rate_constant_dict, marker_species_shorthand, excitation_set)
-#         kinetiscope_reaction_list.extend(chemical_reaction_list)
-#     else:
-#         reaction = handle_phase2_chemical_reactions(H_rxn, mpculeid_name_dict, rate_constant_dict, marker_species_shorthand)
-#         kinetiscope_reaction_list.append(reaction)
+for H_rxn in chemical_reaction_list:
+    if H_rxn.phase == 1:
+        chemical_reaction_list, excitation_set = \
+            handle_phase1_chemical_reactions(H_rxn, mpculeid_name_dict, rate_constant_dict, marker_species_shorthand, excitation_set)
+        kinetiscope_reaction_list.extend(chemical_reaction_list)
+    else:
+        reaction = handle_phase2_chemical_reactions(H_rxn, mpculeid_name_dict, rate_constant_dict, marker_species_shorthand)
+        kinetiscope_reaction_list.append(reaction)
 
 
-# for index, reaction in enumerate(kinetiscope_reaction_list):
-#     if reaction_has_duplicate_species(reaction):
-#         kinetiscope_reaction_list[index] = correct_kinetiscope_name(reaction)
+for index, reaction in enumerate(kinetiscope_reaction_list):
+    if reaction_has_duplicate_species(reaction):
+        kinetiscope_reaction_list[index] = correct_kinetiscope_name(reaction)
+        print(reaction.kinetiscope_name)
         
-#     if "proton_coupled_electron_transfer" in reaction.marker_species:
-#         kinetiscope_reaction_list[index] = shorten_PCET(reaction)
-    
-# class DuplicateReactionError(Exception):
-#     """Custom exception for duplicate reactions."""
-#     def __init__(self, name, count):
-#         self.name = name
-#         self.count = count
-#         super().__init__(f"Duplicate reaction found: '{name}' appears {count} times.")
+    if "proton_coupled_electron_transfer" in reaction.marker_species:
+        kinetiscope_reaction_list[index] = shorten_PCET(reaction)
 
-# def remove_duplicate_reactions(kinetiscope_reaction_list):
-#     new_list = []
-#     name_set = set()
-#     for reaction in kinetiscope_reaction_list:
-#         if reaction.kinetiscope_name not in name_set:
-#             new_list.append(reaction)
-#             name_set.add(reaction.kinetiscope_name)
-#     return new_list
-
-# def count_kinetiscope_names(kinetiscope_reaction_list):
-#     # Dictionary to store the count of each kinetiscope_name
-#     name_counts = {}
-
-#     # Iterate over the list of reactions
-#     for reaction in kinetiscope_reaction_list:
-#         # Retrieve kinetiscope_name
-#         kinetiscope_name = reaction.kinetiscope_name
-        
-#         # Update the count in the dictionary
-#         if kinetiscope_name in name_counts:
-#             name_counts[kinetiscope_name] += 1
-#         else:
-#             name_counts[kinetiscope_name] = 1
-
-#     # Check for duplicates and raise an error if any are found
-#     for name, count in name_counts.items():
-#         if count > 1:
-#             raise DuplicateReactionError(name, count)
-
-# # Example usage:
-# try:
-#     kinetiscope_reaction_list = remove_duplicate_reactions(kinetiscope_reaction_list)
-#     count_kinetiscope_names(kinetiscope_reaction_list)
-# except DuplicateReactionError as e:
-#     print(f"Error: {e}")
-    
-# dumpfn(kinetiscope_reaction_list, "kinetiscope_full_reaction_list.json")
-        
 # supercategory_order = [
 #     "absorption", "electron_ionization", "recombination", "attachment", "excitation",
 #     "dexcitation", "fragmentation", "isomerization", "ion-ion", "ion-molecule", "neutral"]
@@ -324,10 +299,34 @@ for rxn_list in HiPRGen_reaction_list["ionization"].values():
 
 # # os.chdir(new_dir)
     
-# with open("euvl_full_reactions_fixed_081324.csv", 'w', newline = "") as csvfile:
+# with open("euvl_full_reactions.csv", 'w', newline = "") as csvfile:
 #     writer = csv.DictWriter(csvfile, fieldnames = dict_list[0].keys())
 #     writer.writeheader()
 #     for reaction in dict_list:
 #         writer.writerow(reaction)
   
 # print('Done!') 
+    
+#         A dictionary with kinetiscope_names as keys and their counts as values.
+#     """
+#     # Dictionary to store the count of each kinetiscope_name
+#     name_counts = {}
+
+#     # Iterate over the list of reactions
+#     for reaction in kinetiscope_reaction_list:
+#         # Retrieve kinetiscope_name
+#         kinetiscope_name = reaction.kinetiscope_name
+        
+#         # Update the count in the dictionary
+#         if kinetiscope_name in name_counts:
+#             name_counts[kinetiscope_name] += 1
+#         else:
+#             name_counts[kinetiscope_name] = 1
+
+#     for name, count in name_counts.items():
+#         if count > 1:
+#             print(f"{name}: {count}")
+
+# counts = count_kinetiscope_names(kinetiscope_reaction_list)
+# dumpfn(kinetiscope_reaction_list, "kinetiscope_full_reaction_list.json")
+        
